6460dd8c92476d50dddbc179d4ca9a50239687b1
Alex <alexlanxy@gmail.com>
2025-04-29 22:33:27 -0500

    git commit -m "Remove evaluate_past_experiments directory from tracking"

--- evaluate_past_experiments/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Evaluation package for JobExtractX model.
-
-This package contains tools for evaluating model performance 
-on JSON extraction tasks from job descriptions.
-"""
-
-__version__ = "0.1.0"
